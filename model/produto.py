852521d9bdee9f76e17cb04ac83ff02c68fa2a81
Saymon Reis <saymon.reis@hotmail.com>
2024-07-10 16:42:42 -0300

    Inicio api

---
+++ model/produto.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, String, Integer, DateTime, Float
+from datetime import datetime
+from typing import Union
+
+from  model import Base
+
+
+class Produto(Base):
+    __tablename__ = 'produto'
+
+    id = Column("pk_produto", Integer, primary_key=True)
+    nome = Column(String(200))
+    observacoes = Column(String(4000))
+    marca = Column(String(200))
+    url_produto = Column(String(2048))
+    valor = Column(Float)
+    id_mercado_livre = Column(String(200))
+    quantidade = Column(Integer)
+    condicao = Column(String(10))
+    url_imagem = Column(String(2048))
+    data_registro = Column(DateTime, default=datetime.now())
+
+    def __init__(self, 
+                    nome: str,
+                    observacoes: str,
+                    marca: str,
+                    url_produto: str,
+                    valor: float,
+                    id_mercado_livre: str,
+                    quantidade: int,
+                    condicao: str,
+                    url_imagem: str,
+                    data_registro:Union[DateTime, None] = None):
+        """
+        Cria um Produto
+
+        Arguments:
+            nome: nome do troduto,
+            observacoes: informações adicionais,
+            marca: marca do produto,
+            url_produto: url do produto no site Mercado Livre,
+            valor: valor do produto,
+            id_mercado_livre: id do produto no Mercado Livre,
+            quantidade: quantidade disponível a venda,
+            condicao: produto novo ou usado,
+            url_imagem: url da imagem do produto no site Mercado Livre,
+            data_registro: data de quando o produto foi inserido à base
+        """
+        self.nome = nome
+        self.observacoes = observacoes
+        self.marca = marca
+        self.url_produto = url_produto
+        self.valor = valor
+        self.id_mercado_livre = id_mercado_livre
+        self.quantidade = quantidade
+        self.condicao = condicao
+        self.url_imagem = url_imagem
+        if data_registro:
+            self.data_registro = data_registro
---
+++ schemas/produto.py
@@ -0,0 +1,64 @@
+from unicodedata import category
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class ProdutoSchema(BaseModel):
+    nome: str = "Kit 5 Camisetas Básicas Masculina Dry Fit Lisa Tradicional"
+    observacoes: str = "Camisetas para revenda"
+    marca: str = "Veronz"
+    url_produto: str = "https://produto.mercadolivre.com.br/MLB-2918685347-kit-5-camisetas-basicas-masculina-dry-fit-lisa-tradicional-_JM"
+    valor: float = 63.12
+    id_mercado_livre: str = "MLB2918685347"
+    quantidade: int = 500
+    condicao: str = "new"
+    url_imagem: str = "http://http2.mlstatic.com/D_693684-MLB54835234994_042023-I.jpg"
+    data_registro: str = "04/07/2024"
+
+
+class ProdutoBuscaSchema(BaseModel):
+    id: int = 1
+
+
+class ProdutoViewSchema(BaseModel):
+    id: int = 1
+    nome: str = "Kit 5 Camisetas Básicas Masculina Dry Fit Lisa Tradicional"
+    observacoes: str = "Camisetas para revenda"
+    marca: str = "Veronz"
+    url_produto: str = "https://produto.mercadolivre.com.br/MLB-2918685347-kit-5-camisetas-basicas-masculina-dry-fit-lisa-tradicional-_JM"
+    valor: float = 63.12
+    id_mercado_livre: str = "MLB2918685347"
+    quantidade: int = 500
+    condicao: str = "new"
+    url_imagem: str = "http://http2.mlstatic.com/D_693684-MLB54835234994_042023-I.jpg"
+    data_registro: str = "04/07/2024"
+
+class ProdutoDelSchema(BaseModel):
+    mesage: str
+    id: int
+
+def apresenta_produto(produto: ProdutoViewSchema):
+    return {
+        "id": produto.id,
+        "nome": produto.nome,
+        "observacoes": produto.observacoes,
+        "marca": produto.marca,
+        "url_produto": produto.url_produto,
+        "valor": produto.valor,
+        "id_mercado_livre": produto.id_mercado_livre,
+        "quantidade": produto.quantidade,
+        "condicao": produto.condicao,
+        "url_imagem": produto.url_imagem,
+        "data_registro": produto.data_registro
+    }
+
+
+class ProdutoListaViewSchema(BaseModel):
+    produtos: List[ProdutoViewSchema]
+
+
+def apresenta_lista_produto(produtos):
+    result = []
+    for produto in produtos:
+        result.append(apresenta_produto(produto))
+    return {"produtos": result}
